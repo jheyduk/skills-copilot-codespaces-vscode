f5f605cecfc8384c0640c5fac6c07810883a887a
jheyduk <jheyduk@users.noreply.github.com>
2023-07-17 21:49:19 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,124 @@
+function skillsMember() {
+    var skills = [
+        {
+            name: "JavaScript",
+            level: "Advanced"
+        },
+        {
+            name: "Java",
+            level: "Advanced"
+        },
+        {
+            name: "HTML",
+            level: "Advanced"
+        },
+        {
+            name: "CSS",
+            level: "Advanced"
+        },
+        {
+            name: "PHP",
+            level: "Advanced"
+        },
+        {
+            name: "SQL",
+            level: "Advanced"
+        },
+        {
+            name: "C#",
+            level: "Advanced"
+        },
+        {
+            name: "C++",
+            level: "Advanced"
+        },
+        {
+            name: "Python",
+            level: "Advanced"
+        },
+        {
+            name: "Ruby",
+            level: "Advanced"
+        },
+        {
+            name: "C",
+            level: "Advanced"
+        },
+        {
+            name: "Node.js",
+            level: "Advanced"
+        },
+        {
+            name: "jQuery",
+            level: "Advanced"
+        },
+        {
+            name: "Angular.js",
+            level: "Advanced"
+        },
+        {
+            name: "React.js",
+            level: "Advanced"
+        },
+        {
+            name: "Express.js",
+            level: "Advanced"
+        },
+        {
+            name: "MongoDB",
+            level: "Advanced"
+        },
+        {
+            name: "Bootstrap",
+            level: "Advanced"
+        },
+        {
+            name: "WordPress",
+            level: "Advanced"
+        },
+        {
+            name: "Django",
+            level: "Advanced"
+        },
+        {
+            name: "Laravel",
+            level: "Advanced"
+        },
+        {
+            name: "React Native",
+            level: "Advanced"
+        },
+        {
+            name: "Android",
+            level: "Advanced"
+        },
+        {
+            name: "iOS",
+            level: "Advanced"
+        },
+        {
+            name: "Swift",
+            level: "Advanced"
+        },
+        {
+            name: "Objective-C",
+            level: "Advanced"
+        },
+        {
+            name: "Kotlin",
+            level: "Advanced"
+        },
+        {
+            name: "Xamarin",
+            level: "Advanced"
+        },
+        {
+            name: "Unity",
+            level: "Advanced"
+        },
+        {
+            name: "Cocos2d",
+            level: "Advanced"
+        }];
+    return skills;
+}
